4811df501e348272e992b0e7ac5549fc270a2e0c
sblantipodi <perini.davide@dpsoftware.org>
2024-11-25 21:22:26 +0100

    flatpak

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp ../../*.deb FireflyLuciferinLinux.deb;
-flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo --install builddir org.dpsoftware.FireflyLuciferin.json
+cd org.dpsoftware.FireflyLuciferin;
+flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo --install builddir org.dpsoftware.FireflyLuciferin.json;
