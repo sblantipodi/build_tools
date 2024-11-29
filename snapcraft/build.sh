62d9fa5b92f0dcc392d6c483ac0805bcca67f63b
sblantipodi <perini.davide@dpsoftware.org>
2024-11-29 11:35:12 +0100

    snapcraft

--- snapcraft/build.sh
+++ snapcraft/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Copyright © 2020 - 2024  Davide Perini  (https://github.com/sblantipodi)
+# If you pass an argument version it uses it without prompting for it. (ex ./built.sh 2.17.10)
+# This install a new build locally.
+# other manifest: https://github.com/search?q=path%3Asnapcraft.yaml+%22icon%22&type=code
 
 if [ -z "$1" ]; then
   read -p "Please insert app version (ex: 2.17.10): " app_version
@@ -7,12 +11,34 @@ else
   app_version=$1
 fi
 
+sed -i "s/VERSION_PLACEHOLDER/$app_version/g" snap/snapcraft.yaml;
+
+
+cd ..;
+cd ..;
+mvn clean;
+mvn -B package;
+echo "";
+
+echo "Running jpackage...";
+jpackage -i target --main-class org.dpsoftware.JavaFXStarter \
+--main-jar FireflyLuciferin-jar-with-dependencies.jar \
+--icon data/img/luciferin_logo.png --linux-shortcut \
+--copyright "Davide Perini" --name FireflyLuciferin \
+--vendor DPsoftware --app-version "$app_version" \
+--java-options "-XX:+UseZGC -XX:+UseStringDeduplication -Xms64m -Xmx1024m \
+--add-modules=jdk.incubator.vector --enable-native-access=org.dpsoftware \
+--enable-native-access=ALL-UNNAMED";
+
+cd build_tools/snapcraft || exit;
+cp ../../*.deb ./FireflyLuciferinLinux.deb;
+rm -rf ../../firef*.deb;
+
 snapcraft clean;
 sudo snap remove fireflyluciferin;
-
-sed -i "s/VERSION_PLACEHOLDER/$app_version/g" snap/snapcraft.yaml;
 
 snapcraft --verbosity=debug;
 sudo snap install fireflyluciferin_${app_version}_amd64.snap --dangerous --devmode;
 snap run fireflyluciferin;
 sed -i "s/$app_version/VERSION_PLACEHOLDER/g" snap/snapcraft.yaml;
+rm -rf FireflyLuciferinLinux.deb;
